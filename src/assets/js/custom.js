703de3fb0dc439955aed1304a1e32212c561367b
Chris Healy <chrishealy871@gmail.com>
2017-05-21 22:35:58 +0100

    Finsished working version

---
+++ src/assets/js/custom.js
@@ -0,0 +1,41 @@
+/**
+ * Created by chrishealy on 21/05/2017.
+ */
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = 0; // For IE and Firefox
+
+}
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: 'smooth'
+});
+
+// Scroll to a certain element
+document.querySelector('.hello').scrollIntoView({
+  behavior: 'smooth'
+});
