2b588e74d0acb48e4bfc37ee1da724cc1196870d
Chris Healy <chrishealy871@gmail.com>
2017-05-21 22:46:18 +0100

    Finsished working version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './app-routing.module';
-import {Ng2PageScrollModule} from 'ng2-page-scroll';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { GiphyComponent } from './giphy/giphy.component';
 import { MainSectionComponent } from './main-section/main-section.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -17,7 +12,6 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    GiphyComponent,
     MainSectionComponent,
     FooterComponent,
   ],
@@ -25,9 +19,7 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes),
     NgbModule.forRoot(),
-    Ng2PageScrollModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
